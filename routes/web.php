39b2df490a8a9f16d1e3934eb21e406cfa12f0e3
krysduda <krystianduda64@gmail.com>
2020-12-29 16:42:45 +0100

    Moved account activation route to web

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Auth\AuthenticationController;
+use App\Http\Controllers\Auth\EmailVerificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -14,3 +15,10 @@ Route::get('/login/github/callback', [AuthenticationController::class, 'callback
 //Facebook
 Route::get('/login/facebook', [AuthenticationController::class, 'redirectToFacebook']);
 Route::get('/login/facebook/callback', [AuthenticationController::class, 'callbackToFacebook']);
+
+//Email Verification
+Route::middleware('auth:sanctum')->prefix('/email')->group(function (): void {
+    Route::get('/verify/{id}/{hash}', [EmailVerificationController::class, 'accept'])
+        ->middleware('signed')
+        ->name('verification.verify');
+});
