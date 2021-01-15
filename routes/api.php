e3043047db506bf0621aa5cd5e2fe7c9bc30d0f1
5david5 <48356242+5david5@users.noreply.github.com>
2021-01-15 17:04:55 +0100

    Updated routes

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Auth\{
     EmailVerificationController,
     ResetPasswordController
 };
+
 use App\Http\Controllers\Ingredients\{
     HopController,
     YeastController,
@@ -23,69 +24,54 @@ use Illuminate\Routing\Router;
 $router = app(Router::class);
 
 // Authentication
-$router->post('/register', [AuthenticationController::class, 'register'])->name('register');
-$router->post('/login', [AuthenticationController::class, 'login'])->name('login');
+$router->post('register', [AuthenticationController::class, 'register'])->name('register');
+$router->post('login', [AuthenticationController::class, 'login'])->name('login');
 
 //Email Verification
-$router->middleware('auth:sanctum')->prefix('/email')->group(function (Router $router): void {
-    $router->get('/verify', [EmailVerificationController::class, 'verify'])
+$router->middleware('auth:sanctum')->prefix('email/')->group(function (Router $router): void {
+    $router->get('verify', [EmailVerificationController::class, 'verify'])
         ->name('verification.notice');
 
-    $router->post('/verification-notification', [EmailVerificationController::class, 'resend'])
+    $router->post('verification-notification', [EmailVerificationController::class, 'resend'])
         ->middleware('throttle:6,1')
         ->name('verification.send');
 });
 
 //Reset Password
 $router->middleware('guest')->group(function (Router $router): void {
-    $router->post('/forgot-password', [ResetPasswordController::class, 'forgotPassword'])->name('password.email');
-    $router->get('/reset-password/{token}', [ResetPasswordController::class, 'getToken'])->name('password.reset');
-    $router->post('/reset-password', [ResetPasswordController::class, 'resetPassword'])->name('password.update');
+    $router->post('forgot-password', [ResetPasswordController::class, 'forgotPassword'])->name('password.email');
+    $router->get('reset-password/{token}', [ResetPasswordController::class, 'getToken'])->name('password.reset');
+    $router->post('reset-password', [ResetPasswordController::class, 'resetPassword'])->name('password.update');
 });
 
 // Authorization
 $router->middleware(['auth:sanctum', 'verified'])->group(function (Router $router): void {
-    $router->get('/user', function (Request $request) {
+    $router->get('user', function (Request $request) {
         return $request->user();
     });
-    $router->post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
 
-    $router->get('/extra-type', [ExtraController::class, 'types']);
-    $router->get('/fermentable-type', [FermentableController::class, 'types']);
-    $router->get('/yeast-type', [YeastController::class, 'types']);
+    //Ingredients
+    $router->apiResource('hops', HopController::class);
+    $router->apiResource('yeasts', YeastController::class);
+    $router->apiResource('fermentables', FermentableController::class);
+    $router->apiResource('extras', ExtraController::class);
 
-    $router->get('hops', [HopController::class, 'index']);
-    $router->get('hops/{hop}', [HopController::class, 'show']);
-    $router->post('hops', [HopController::class, 'store']);
-    $router->put('hops/{hop}', [HopController::class, 'update']);
-    $router->delete('hops/{hop}', [HopController::class, 'destroy']);
+    //Types
+    $router->get('extra-type', [ExtraController::class, 'types']);
+    $router->get('fermentable-type', [FermentableController::class, 'types']);
+    $router->get('yeast-type', [YeastController::class, 'types']);
 
-    $router->get('yeasts', [YeastController::class, 'index']);
-    $router->get('yeasts/{yeast}', [YeastController::class, 'show']);
-    $router->post('yeasts', [YeastController::class, 'store']);
-    $router->put('yeasts/{yeast}', [YeastController::class, 'update']);
-    $router->delete('yeasts/{yeast}', [YeastController::class, 'destroy']);
-
-    $router->get('fermentables', [FermentableController::class, 'index']);
-    $router->get('fermentables/{fermentable}', [FermentableController::class, 'show']);
-    $router->post('fermentables', [FermentableController::class, 'store']);
-    $router->put('fermentables/{fermentable}', [FermentableController::class, 'update']);
-    $router->delete('fermentables/{fermentable}', [FermentableController::class, 'destroy']);
-
-    $router->get('extras', [ExtraController::class, 'index']);
-    $router->get('extras/{extra}', [ExtraController::class, 'show']);
-    $router->post('extras', [ExtraController::class, 'store']);
-    $router->put('extras/{extra}', [ExtraController::class, 'update']);
-    $router->delete('extras/{extra}', [ExtraController::class, 'destroy']);
-
+    //Notifications
     $router->get('notifications', [IngredientExpirationController::class, 'index']);
     $router->get('unread-Notifications', [IngredientExpirationController::class, 'getUnread']);
     $router->get('number-Of-Unread-Notifications', [IngredientExpirationController::class, 'getNumberOfUnread']);
     $router->put('notifications/{notification}', [IngredientExpirationController::class, 'read']);
     $router->delete('notifications/{notification}', [IngredientExpirationController::class, 'destroy']);
 
+    //User Actions
     $router->put('settings', [UserSettingsController::class, 'update']);
     $router->get('settings', [UserSettingsController::class, 'show']);
     $router->post('change-password', [ChangePasswordController::class, 'update']);
     $router->get('logs', [UserLogsController::class, 'index']);
+    $router->post('logout', [AuthenticationController::class, 'logout'])->name('logout');
 });
--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,16 @@ $router = app(Router::class);
 $router->view('/', 'welcome');
 
 //GitHub
-$router->get('/login/github', [SocialiteAuthController::class, 'redirectToGithub']);
-$router->get('/login/github/callback', [SocialiteAuthController::class, 'callbackToGithub']);
+$router->get('login/github', [SocialiteAuthController::class, 'redirectToGithub']);
+$router->get('login/github/callback', [SocialiteAuthController::class, 'callbackToGithub']);
 
 //Facebook
-$router->get('/login/facebook', [SocialiteAuthController::class, 'redirectToFacebook']);
-$router->get('/login/facebook/callback', [SocialiteAuthController::class, 'callbackToFacebook']);
+$router->get('login/facebook', [SocialiteAuthController::class, 'redirectToFacebook']);
+$router->get('login/facebook/callback', [SocialiteAuthController::class, 'callbackToFacebook']);
 
 //Email Verification
-$router->middleware('auth:sanctum')->prefix('/email')->group(function (Router $router): void {
-    $router->get('/verify/{id}/{hash}', [EmailVerificationController::class, 'accept'])
+$router->middleware('auth:sanctum')->prefix('email/')->group(function (Router $router): void {
+    $router->get('verify/{id}/{hash}', [EmailVerificationController::class, 'accept'])
         ->middleware('signed')
         ->name('verification.verify');
 });
